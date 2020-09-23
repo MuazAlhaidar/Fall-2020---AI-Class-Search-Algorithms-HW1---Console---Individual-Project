b36a012eb910189ae77c3d88823eef3374f68db9
MuazAlhaidar <omunam10@gmail.com>
2020-09-23 17:09:07 -0400

    adding HW2 pdf and cleaning up main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,43 +1,7 @@
-#include "Astar.hpp"
-#include "IterativeDeepeningSearch_IDS.hpp"
 #include <iostream>
-#include <vector>
 using namespace std;
-
-void aastar() {
-
-    point _start(3, 0);
-    point end(2, 2);
-
-    node start(_start);
-    // point end(4,4);
-    vector<vect> test = astar(&start, end);
-    time_t now = time(0);
-    struct tm tstruct;
-    tstruct = *localtime(&now);
-
-    std::cout << "Muaz Alhaidar(omunam) and Zakariya Ahmed(zakahmed) - Time: " << tstruct.tm_hour - 12 << ":" << tstruct.tm_min << ":" << tstruct.tm_sec << std::endl;
-    cout << "A* Search" << endl;
-    string line = "--------------";
-    cout << line << endl;
-    for (auto row : test) {
-        for (auto x : row) {
-            if (x.label == -1) {
-                cout << "##" << ' ';
-            } else {
-                if (x.label < 10 && x.label >= 0) {
-                    cout << '0';
-                }
-                cout << x.label << ' ';
-            }
-        }
-        cout << endl;
-    }
-    cout << line << endl;
-}
 
 int main() {
 
-    iterativeDeepeningDFS();
-    aastar();
+    return 0;
 }
